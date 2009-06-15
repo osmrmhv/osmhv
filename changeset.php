f1561b6c37980d827fcd30c364d76f4da98bb887
Candid Dauth <cdauth@cdauth.de>
2009-06-15 21:33:57 +0000

    OpenStreetBugs z-index.

--- changeset.php
+++ changeset.php
@@ -137,7 +137,9 @@
 	var styleMapCreated = new OpenLayers.StyleMap({strokeColor: "#44ff44", strokeWidth: 3, strokeOpacity: 0.5});
 	var styleMapRemoved = new OpenLayers.StyleMap({strokeColor: "#ff0000", strokeWidth: 3, strokeOpacity: 0.5});
 
-	map.addLayer(new OpenLayers.Layer.cdauth.markers.OpenStreetBugs("OpenStreetBugs"));
+	var osbLayer = new OpenLayers.Layer.cdauth.markers.OpenStreetBugs("OpenStreetBugs", "../map/openstreetbugs.php", { visibility: false });
+	map.addLayer(osbLayer);
+	osbLayer.setZIndex(500);
 
 	var layerMarkers = new OpenLayers.Layer.cdauth.markers.LonLat("Markers");
 	map.addLayer(layerMarkers);
