4daff2cd3a7cb57087be566d4ed0b28d9255dca8
Candid Dauth <cdauth@cdauth.de>
2009-10-31 18:42:28 +0000

    Tag differences are now displayed.

--- changeset.php
+++ changeset.php
@@ -85,8 +85,8 @@
 	}
 ?>
 <noscript><p><strong><?=htmlspecialchars(_("Note that many features of this page will not work without JavaScript."))?></strong></p></noscript>
-<p>
 <p><?=sprintf(htmlspecialchars(_("This analysation was created on %s.")), gmdate("Y-m-d\\TH:i:s\\Z", $information["analysed"]))?></p>
+<p class="introduction"><strong><?=htmlspecialchars(_("Everything green on this page will show the status after the changeset was committed, red will be the status before, and things displayed in blue haven’t changed."))?></strong></p>
 <h2><?=htmlspecialchars(_("Tags"))?></h2>
 <dl>
 <?php
@@ -125,6 +125,104 @@
 	<dt><?=htmlspecialchars(_("User"))?></dt>
 	<dd><a href="http://www.openstreetmap.org/user/<?=htmlspecialchars(rawurlencode($information["user"]))?>"><?=htmlspecialchars($information["user"])?></a></dd>
 </dl>
+<h2><?=htmlspecialchars(_("Changed object tags"))?></h2>
+<p class="changed-object-tags-note"><?=htmlspecialchars(_("Hover the elements to view the changed tags."))?></p>
+<ul class="changed-object-tags">
+<?php
+	$old_type = null;
+	$old_id = null;
+	$tags = $sql->query("SELECT * FROM changeset_tags_objects WHERE changeset = ".$sql->quote($_GET["id"]).";");
+	while($line = $tags->fetch())
+	{
+		if($line["type"] != $old_type || $line["id"] != $old_id)
+		{
+			if($old_type !== null)
+			{
+?>
+		</table>
+	</li>
+<?php
+			}
+
+			switch($line["type"])
+			{
+				case 1:
+					$type = _("Node");
+					$browse = "node";
+					break;
+				case 2:
+					$type = _("Way");
+					$browse = "way";
+					break;
+				case 3:
+					$type = _("Relation");
+					$browse = "relation";
+					break;
+			}
+?>
+	<li><?=htmlspecialchars($type." ".$line["id"])?> (<a href="http://www.openstreetmap.org/browse/<?=htmlspecialchars($browse."/".$line["id"])?>"><?=htmlspecialchars(_("browse"))?></a>)
+		<table>
+			<tbody>
+<?php
+			$old_type = $line["type"];
+			$old_id = $line["id"];
+		}
+
+		if($line["value1"] == $line["value2"])
+		{
+			$class1 = "unchanged";
+			$class2 = "unchanged";
+		}
+		else
+		{
+			$class1 = "old";
+			$class2 = "new";
+		}
+
+		$values = array();
+		foreach(array($line["value1"], $line["value2"]) as $i=>$v)
+		{
+			if(trim($v) != "")
+			{
+				if(preg_match("/^url(:|\$)/i", $line["tagname"]))
+				{
+					$v = explode(";", $v);
+					foreach($v as $k=>$v1)
+						$v[$k] = "<a href=\"".htmlspecialchars(trim($v1))."\">".htmlspecialchars($v1)."</a>";
+					$v = implode(";", $v);
+				}
+				elseif(preg_match("/^wiki(:.*)?\$/i", $line["tagname"], $m))
+				{
+					$m[1] = strtolower($m[1]);
+					$v = explode(";", $v);
+					foreach($v as $k=>$v1)
+						$v[$k] = "<a href=\"http://wiki.openstreetmap.org/wiki/".htmlspecialchars(rawurlencode(($m[1] == ":symbol" ? "Image:" : "").$v1))."\">".htmlspecialchars($v1)."</a>";
+					$v = implode(";", $v);
+				}
+				else
+					$v = htmlspecialchars($v);
+			}
+			$values[$i] = $v;
+		}
+?>
+				<tr>
+					<th><?=htmlspecialchars($line["tagname"])?></th>
+					<td class="<?=htmlspecialchars($class1)?>"><?=$values[0]?></td>
+					<td class="<?=htmlspecialchars($class2)?>"><?=$values[1]?></td>
+				</tr>
+<?php
+	}
+
+	if($old_type !== null)
+	{
+?>
+			</tbody>
+		</table>
+	</li>
+<?php
+	}
+?>
+</ul>
 <div id="map"></div>
 <script type="text/javascript">
 // <![CDATA[
@@ -151,18 +249,18 @@
 	var projection = new OpenLayers.Projection("EPSG:4326");
 	var layerCreated = new OpenLayers.Layer.PointTrack("(Created)", {
 		styleMap: styleMapCreated,
-		projection: projection
-		//displayInLayerSwitcher: false
+		projection: projection,
+		zoomableInLayerSwitcher: true
 	});
 	var layerRemoved = new OpenLayers.Layer.PointTrack("(Removed)", {
 		styleMap: styleMapRemoved,
-		projection: projection
-		//displayInLayerSwitcher: false
+		projection: projection,
+		zoomableInLayerSwitcher: true
 	});
 	var layerUnchanged = new OpenLayers.Layer.PointTrack("(Unchanged)", {
 		styleMap: styleMapUnchanged,
-		projection: projection
-		//displayInLayerSwitcher: false
+		projection: projection,
+		zoomableInLayerSwitcher: true
 	});
 <?php
 	$segments = $sql->query("SELECT * FROM changeset_changes WHERE changeset = ".$sql->quote($_GET["id"])." AND action = 1;");
