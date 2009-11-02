4ebdeb6dca5126026e5e659c26e65b43d0c983d9
Candid Dauth <cdauth@cdauth.de>
2009-11-02 01:09:19 +0000

    Fixed some displaying errors.

--- changeset.php
+++ changeset.php
@@ -43,9 +43,7 @@
 		$GUI->option("title", sprintf(_("Changeset %s"), $_GET["id"]));
 
 	$GUI->option("importJavaScript", array(
-		"http://www.openlayers.org/dev/OpenLayers.js",
-		"http://www.openstreetmap.org/openlayers/OpenStreetMap.js",
-		"http://opentiles.com/nop/opentiles.js",
+		"http://www.openlayers.org/api/OpenLayers.js",
 		"http://maps.google.com/maps?file=api&v=2&key=ABQIAAAApZR0PIISH23foUX8nxj4LxT_x5xGo0Rzkn1YRNpahJvSZYku9hTJeTmkeyXv4TuaU5kM077xJUUM7w",
 		"http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=cdauths-map",
 		"http://osm.cdauth.de/map/prototypes.js",
@@ -149,6 +147,7 @@
 				if($old_type !== null)
 				{
 ?>
+			</tbody>
 		</table>
 	</li>
 <?php
